c0040e15a6c20b1bf6a75e8262d627ffe5de12e2
Sheviny <2187357@talnet.nl>
2025-02-05 10:58:26 +0100

    Home page bestanden.

---
+++ HomePage/home.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Navbar met Afbeeldingen</title>
+  <link rel="stylesheet" href="../CSS/home.css">
+</head>
+<body>
+  <div class="navbar">
+    
+    <a href="#">
+      <img src="../Logo/want_more_logo.png" alt="Logo">
+    </a>
+    
+    <a href="#">
+      <img src="../images/buy-home.png" alt="Home">
+    </a>
+    
+    <a href="">
+      Log In
+    </a>
+    <a href="">
+      sign in
+    </a>
+    <a href="#">
+      <img src="../images/contact.png" alt="Contact">
+    </a>
+  </div>
+</body>
+</html>
