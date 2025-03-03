0103d05e5856788bbf2d729289adbe629440c5d2
Sheviny <2187357@talnet.nl>
2025-03-03 15:48:10 +0100

    contact us begin

---
+++ Contact/contact.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact us</title>
+  <link rel="stylesheet" href="../CSS/home.css">
+  <link rel="stylesheet" href="../CSS/contact.css">
+</head>
+<body>
+<div class="header">
+  <div class="container">
+    <div class="navbar">
+      <div class="logo">
+        <a href="../HomePage/home.php">
+          <img src="../Logo/WantMore..png" alt="logo" width="125px">
+        </a>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="../HomePage/home.php">Home</a></li>
+          <li><a href="../Product/product-view-user.php">Producten</a></li>
+          <li><a href="../HomePage/about.html">About</a></li>
+          <li><a href="../Contact/contact.php">Contact</a></li>
+          <li><a href="../user/login-user.php">Account</a></li>
+        </ul>
+      </nav>
+      <img src="../images/shopping-bag.png" width="30px" height="30px" alt="shopping bag">
+    </div>
+
+<h1 class="contact">Contact Us!</h1>
+
+<div class="form">
+<h3>Send Us A Message!</h3>
+
+<div class="input">
+
+<input type="text" id="name" name="name" placeholder="Name*" required>
+</div>
+
+<div class="input">
+<input type="email" name="email" id="email" placeholder="Email*" required>
+</div>
+
+<div class="message">
+  <label for="message">Message</label>
+  <textarea name="message" id="message" placeholder="Type message here.."></textarea>
+</div>
+
+<input type="submit" value="Submit">
+
+</div>
+
+<div class="our-info">
+  <strong><h2>Our Adress</h2></strong>
+</div>
+
+
+
+  
+</body>
+</html>
