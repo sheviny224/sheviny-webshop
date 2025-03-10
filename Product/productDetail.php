aa85861fd386ced7eca7baf43dbcc1c5cf5b89d8
Sheviny <2187357@talnet.nl>
2025-03-10 14:10:53 +0100

    detail page

--- Product/productDetail.php
+++ Product/productDetail.php
@@ -19,7 +19,7 @@ if (!$productDetail) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= htmlspecialchars($productDetail['productNaam']); ?></title>
-  <link rel="stylesheet" href="../CSS/home2.css">
+  <link rel="stylesheet" href="../CSS/home3.css">
 </head>
 <body>
 
